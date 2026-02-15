2738942ff8621e5ac62b023904569c32f14277d2
lulca-a <contato.luca.luca@gmail.com>
2026-02-15 15:55:13 -0300

    if para caso nao ache a carta

--- main.py
+++ main.py
@@ -2,12 +2,16 @@ from scrypull import pull
 from trad import traduzir
 from tratar_entrada import tratar
 
-carta= tratar(["20 Sol Ring"])
+carta= tratar(["1 Daybreak Ranger"])
 carta_pull = pull(carta.titulo())#input tratado, carta encontrada
-print(carta_pull)
-print(' ')
-titulo_en = carta_pull['nome']
-print(titulo_en)
-print('')
-titulo_pt = traduzir(carta_pull)
-print(titulo_pt)
+
+if carta_pull != None:
+    print(carta_pull)
+    print(' ')
+    titulo_en = carta_pull['nome']
+    print(titulo_en)
+    print('')
+    titulo_pt = traduzir(carta_pull)#carta traduzida
+    print(titulo_pt)
+else:
+    print('carta nao encontrada')

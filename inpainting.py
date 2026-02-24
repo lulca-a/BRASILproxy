c55873d833ab7b47a57374aaafc304e4ce4482a8
no-not-salai <contato.luca.luca@gmail.com>
2026-02-24 12:02:35 -0300

    fixed cv2

--- inpainting.py
+++ inpainting.py
@@ -7,43 +7,41 @@ from io import BytesIO
 img = cv2.imread("karlov.png")
 x1, y1 = 58,593
 x2, y2 = 622,635
-h, w = img.shape[:2]
 
 bloco_texto = np.array([
-    [int(0.08*w), int(0.63*h)],
-    [int(0.92*w), int(0.63*h)],
-    [int(0.08*w), int(0.88*h)],
-    [int(0.92*w), int(0.88*h)]
-])
-
+    [60,656],    # 1 topo esquerdo
+    [680,656],   # 2 topo direito
+    [680,915],
+    [570,915],   
+    [570,935],   # 4 canto inferior direito inclinado
+    [60,935],    # 5 canto inferior esquerdo
+         # 6 subindo lado esquerdo
+], dtype=np.int32)
 
 bloco_tipo=[(58,593),(622,635)]
 
 bloco_nome=[(60,60),(610,100)]
 
-
 def recorte(bloco):
-    # caminho da carta
-    
 
-    # cria máscara preta (mesmo tamanho da imagem)
     mask = np.zeros(img.shape[:2], dtype=np.uint8)
-    mask = cv2.GaussianBlur(mask, (21,21), 0)
 
-    # desenha retângulo branco na máscara (área a remover)
+    # desenha polígono
     cv2.fillPoly(mask, [bloco], 255)
-    # aplica inpainting
+
+    # suaviza borda
+    # mask = cv2.GaussianBlur(mask, (21,21), 0)
+
+    # inpainting
     resultado = cv2.inpaint(img, mask, 3, cv2.INPAINT_TELEA)
+    debug = img.copy()
+    cv2.polylines(debug, [bloco_texto], True, (0,0,255), 3)
+    # mistura pra ficar natural
+    alpha = 0.6
+    # suave = cv2.addWeighted(resultado, alpha, img, 1-alpha, 0)
 
-    alpha = 0.6  # força do inpainting (0 = original, 1 = total)
-    suave = cv2.addWeighted(resultado, alpha, img, 1-alpha, 0)
-
-
-    # cv2.imshow("Original", img)
-    # cv2.imshow("Mascara", mask)
-    # cv2.imshow("Inpainting", resultado)
     cv2.imwrite("resultado.png", resultado)
-
+    cv2.imwrite("polly.png", debug)
 recorte(bloco_texto)
 
 # cv2.waitKey(0)
